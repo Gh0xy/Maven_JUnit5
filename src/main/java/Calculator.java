917afa8e3d1c423204cbcb2eb207863bb55e497a
Gh0xy <gh0xy@pm.me>
2022-04-08 20:01:00 +0200

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b; // change + for - for bugged code
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
